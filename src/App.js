0d5ba37f62b1409ebff6edec8ff962d72eddcf14
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-09 01:24:38 -0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Layout>
+        <Sider
+          style={{
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}></Menu>
+        </Sider>
+        <Layout className="site-layout" style={{ marginLeft: 200 }}>
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            <h1 style={{ color: "white", fontSize: "30px" }}>Outlinr</h1>
+          </Header>
+          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, textAlign: "center" }}
+            ></div>
+          </Content>
+          <Footer style={{ textAlign: "center", fontSize: "20px" }}>
+            Outlinr is made with &lt;3 by Pranav :){" "}
+          </Footer>
+        </Layout>
+      </Layout>
+      ,
     </div>
   );
 }
