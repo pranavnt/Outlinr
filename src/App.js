710b19035e83488b5147ac43879e2c682273fb20
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-19 13:42:06 -0800

    fixed imports

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "antd/dist/antd.css";
 import { Layout, Collapse } from "antd";
 
 const { Panel } = Collapse;
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
   return (
--- src/components/Conclusion.js
+++ src/components/Conclusion.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "antd";
+import { Row, Col, Card } from "antd";
 
 export default class Introduction extends React.Component {
   render() {
